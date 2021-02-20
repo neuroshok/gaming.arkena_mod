9bb03e169503075b860e0167a5a7b459501909d6
AymericDev <m.aymeric@gmail.com>
2021-02-20 19:56:24 +0100

    [tool] Hooker with type printing

--- tools/hooker.js
+++ tools/hooker.js
@@ -1,37 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 const hookerProtectedMethodsFile = __dirname + "/.hookerProtectedMethod"
+
+
+const printTypeMapping = {
+	'PlayerControl*': `varName->PlayerId`
+}
+
+const paramListReducer = (acc, e) => `${acc}${acc ? ', ' : ''}${(printTypeMapping[e.type] || (e.pointer ? `(uintptr_t)${e.name}` : e.name)).replace('varName', e.name)}`
+const paramPrintReducer = (acc, e) => `${acc}${acc ? ', ' : ''}${e.name}(${e.type}): {}`
+
 try {
 
 	if (process.argv.length < 3) throw "Missing parameters"
-		const filepath = process.argv[2]
+	const filepath = process.argv[2]
 
-	let hti
-	if ((hti = process.argv.indexOf('--hooktype')) !== -1 && process.argv[hti + 1])
-		var hookType = process.argv[hti + 1]
-			else
-		var hookType = "before"
-
-	var ignoreObfuscated = process.argv.indexOf('--ignore-obfuscated') !== -1
-	var ignoreNonVoid = process.argv.indexOf('--ignore-non-void') !== -1
-
-	let htpi
-	if ((htpi = process.argv.indexOf('--hooktype-protected')) !== -1 && process.argv[htpi + 1])
-		var hookTypeProtected = process.argv[htpi + 1]
-			else
-		var hookTypeProtected = "skip"
-
-	let szi
-	if ((szi = process.argv.indexOf('--size')) !== -1 && process.argv[szi + 1])
-		var size = parseInt(process.argv[szi + 1]) || "full"
-			else
-		var size = "full"
-
-	let sti
-	if ((sti = process.argv.indexOf('--start')) !== -1 && process.argv[sti + 1])
-		var start = parseInt(process.argv[sti + 1])
-	else
-		var start = 0
+	let szi, htpi, hti, hookTypeProtected, hookType, size, start, ignoreObfuscated, ignoreNonVoid
+	hookType = (hti = process.argv.indexOf('--hooktype')) !== -1 && process.argv[hti + 1] ? process.argv[hti + 1] : "before"
+	ignoreObfuscated = process.argv.indexOf('--ignore-obfuscated') !== -1
+	ignoreNonVoid = process.argv.indexOf('--ignore-non-void') !== -1
+	hookTypeProtected = ((htpi = process.argv.indexOf('--hooktype-protected')) !== -1 && process.argv[htpi + 1]) ? process.argv[htpi + 1] : "skip"
+	size = ((szi = process.argv.indexOf('--size')) !== -1 && process.argv[szi + 1]) ? parseInt(process.argv[szi + 1]) || "full" : "full"
+	start = ((sti = process.argv.indexOf('--start')) !== -1 && process.argv[sti + 1]) ? parseInt(process.argv[sti + 1]) : 0
 
 	console.log(`\n\n -- Info -- :
 		- --hooktype : ${hookType}
@@ -43,48 +33,58 @@ try {
 	`)
 
 	let protectedMethods = []
-	if (!fs.existsSync(hookerProtectedMethodsFile))  fs.writeFileSync(hookerProtectedMethodsFile, '')
+	if (!fs.existsSync(hookerProtectedMethodsFile)) fs.writeFileSync(hookerProtectedMethodsFile, '')
 	else protectedMethods = fs.readFileSync(hookerProtectedMethodsFile).toString().split("\n").map(e => e.trim()).filter(e => e && e.indexOf('#') === -1)
 
-	if (!fs.existsSync(filepath))  throw `File ${filepath} not found`
+	if (!fs.existsSync(filepath)) throw `File ${filepath} not found`
 	const folder = path.dirname(filepath)
-
-	const brutLines = fs.readFileSync(filepath).toString().split("\n").map(e => e.trim())
-	let strs = []
+	const brutLines = fs.readFileSync(filepath).toString().replace(/\/\*[\s\S]*?\*\//g, '').split("\n").map(e => e.trim())
+	let dataset = []
 	let structName = null
-	for(i in brutLines) {
+	for (let i in brutLines) {
 		const [full, name, name2] = brutLines[i].match(/^struct (\w+) : ark::meta<(\w+),/) || [null, null, null]
-		if (full)
-	    	structName = name
-		else if (ignoreObfuscated && brutLines[i].match(/^(void|[\w:*]+) ([A-Z0-9]{11})\(([^)]+|)\)\s?{\s?.*/))
-			console.log("Obfuskated skiped: ", brutLines[i])
-		else if (ignoreNonVoid && brutLines[i].match(/^(?!void)[\w:*]+ (\w+)\(([^)]+|)\)\s?{\s?.*/))
-			console.log("Non-void skiped: ", brutLines[i])
-		else if (brutLines[i].match(/^[\w:*]+ (\w+)\(([^)]+|)\)\s?{\s?.*/))
-	    	strs.push(brutLines[i])
+		if (full && name === name2)
+			structName = name
+		else if (brutLines[i].match(/^[\w:*]+ (\w+)\(([^)]+|)\)\s?{\s?.*/)) {
+			let json = brutLines[i].replace(/^((struct |)([\w:*]+) ([\w]+)\(([^)]+|)\).*\/\/ (0x[A-F0-9]{6}).*)/, `{"struct": "$2","type": "$3","method": "$4","rawParameters": "$5", "address": "$6", "full": "$1"}`)
+			let data = JSON.parse(json)
+			data.obfuscated = !!data.method.match(/^[A-Z0-9]{11}$/);
+			data.pointer = data.type.slice(-1) === '*';
+			data.struct = !!data.struct;
+			data.parameters = data.rawParameters ? data.rawParameters.split(/,\s?/).map(e => {
+				let param = e.split(' ');
+				return {struct: param[0] === 'struct', pointer: param[param.length - 2].slice(-1) === '*', type: param[param.length - 2], name: param[param.length - 1]}
+			}) : []
+			data.isProtected = ~protectedMethods.indexOf(`${structName}::${data.method}`)
+			console.log(data)
+			if ((ignoreNonVoid && data.type !== 'void') || (ignoreObfuscated && data.obfuscated)) continue;
+			dataset.push(data)
+		}
 	}
-	const results = { inits: [], hooks: [] }
+	if (!structName) throw "Class name not found, pattern : struct NAME : ark::meta<NAME,"
+	const results = {inits: [], hooks: []}
 
-	strs = strs.slice(start, size === "full" ? strs.length : start + size)
+	dataset = dataset.slice(start, size === "full" ? dataset.length : start + size)
 
-	strs.map(str => {
-		let data = JSON.parse(str.replace(/^([\w:*]+) ([\w]+)\(([^)]+|)\).*/, `{"type": "$1","method": "$2","parameters": "$3"}`))
-		let protected = ~protectedMethods.indexOf(`${structName}::${data.method}`)
+	dataset.map(data => {
+		let hook = `ark::hook<&${structName}::${data.method}>::${data.isProtected ? hookTypeProtected : hookType}`
+		hook += `(this, [](auto original, auto&& self${data.rawParameters ? `, ${data.rawParameters}` : ''}) -> ${data.type} {`
+		hook += `original(self, ${data.parameters.reduce((acc, e) => `${acc}${acc ? ', ' : ''}${e.name}`, "")});`
+		hook += `ark_trace("${structName}::${data.method}, ${data.parameters.reduce(paramPrintReducer, "")}, ",${data.parameters.reduce(paramListReducer, "")});`
+		if (data.type !== 'void')
+			hook += ` return ${data.pointer ? 'nullptr' : `${data.type}{}`};`
+		hook += `}); // ${data.address}`
 
-		let hook = `ark::hook<&${structName}::${data.method}>::${protected ? hookTypeProtected : hookType}(this, [](auto&& self${data.parameters ? `, ${data.parameters}` : '' }) -> ${data.type} {ark_trace("${structName}::${data.method} called");${data.type === 'void' ? '' : ` return ${data.type.slice(-1) === '*' ? 'nullptr' : `${data.type}{}`};`}});`
-
-		if (!protected || hookTypeProtected !== 'skip') {
+		if (!data.isProtected || hookTypeProtected !== 'skip') {
 			results.hooks.push(hook)
 			results.inits.push(`hkr(${data.type}, ${structName}::${data.method});`)
 		}
 	})
 
-	console.log(`\n\n -- Hook -- (${results.hooks.length}): \n#include <analysis/${structName}.hooks.hpp>`)
-	console.log(`\n\n -- INITS -- (${results.inits.length}): \n#include <analysis/${structName}.init.hpp>`)
-
-	fs.writeFileSync(`${folder}/../analysis/${structName}.init.hpp`, results.inits.join("\n")); 
-	fs.writeFileSync(`${folder}/../analysis/${structName}.hooks.hpp`, results.hooks.join("\n")); 
-
+	for (const key in results) {
+		console.log(`\n\n -- Hook -- (${results[key].length}): \n#include <analysis/${structName}.hooks.hpp>`)
+		fs.writeFileSync(`${folder}/../analysis/${structName}.${key}.hpp`, results[key].join("\n"));
+	}
 
 } catch (e) {
 	console.log(e)
