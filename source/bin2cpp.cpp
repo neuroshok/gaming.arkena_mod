78afabe73891e8309415e39e7f30a46d08ca5fa9
ads00 <ads.neuroshok@gmail.com>
2021-03-01 22:48:48 +0100

    [update] bin2cpp

--- source/bin2cpp.cpp
+++ source/bin2cpp.cpp
@@ -1,19 +1,23 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <streambuf>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <streambuf>
 
-int main()
+namespace fs = std::filesystem;
+
+void encode(const std::string& resource_name)
 {
-    std::ifstream ifs("../resource/graphic/ark.png", std::ios::binary);
+    std::ifstream ifs("../resource/graphic/" + resource_name + ".png", std::ios::binary);
     if (!ifs.is_open()) throw "file not found";
 
     std::vector<unsigned char> data(std::istreambuf_iterator<char>(ifs), {});
     ifs.close();
 
-    std::ofstream ofs("../source/ark/ui/resource/icon.cpp");
+    std::ofstream ofs("../source/ark/resource/" + resource_name + ".cpp");
 
-    std::string header = "#include <ark/ui/resource.hpp>\n\n";
+    std::string header = "#include <ark/resource.hpp>\n\n";
     header += "static const unsigned char raw_data[] = {\n";
 
     ofs.write(header.data(), header.size());
@@ -25,12 +29,23 @@ int main()
     }
 
     std::string footer = "};\n\n";
-    footer += "static const ark::ui::resource::image icon = { .size = " + std::to_string(data.size()) + ", .data = raw_data };";
+    footer += "ark::ui::image ark::ui::resource::icon = { .size = " + std::to_string(data.size()) + ", .data = raw_data };";
 
 
     ofs.write(footer.data(), footer.size());
 
     ofs.close();
+}
+
+int main()
+{
+    std::string resource_name = "button";
+
+    for (auto& entry : fs::directory_iterator("../resource/graphic/"))
+    {
+        std::cout << "\nProcessing " << entry.path().stem();
+        encode(entry.path().stem().u8string());
+    }
 
     return 0;
 }
