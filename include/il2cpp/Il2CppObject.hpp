06d206d9808ec34d24d04edc5fa19a3aaac8a515
Arkena <ads.neuroshok@gmail.com>
2023-03-18 02:46:50 +0100

    [add] Il2CppObject

---
+++ include/il2cpp/Il2CppObject.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_IL2CPP_IL2CPPOBJECT_HPP_ARKENA_MOD
+#define INCLUDE_IL2CPP_IL2CPPOBJECT_HPP_ARKENA_MOD
+
+// wrapper file included by generated files
+
+#include "core.hpp"
+
+#endif // INCLUDE_IL2CPP_IL2CPPOBJECT_HPP_ARKENA_MOD
