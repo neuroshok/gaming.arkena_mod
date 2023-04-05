c7866758f48487271d6e9a2107e09125ae52f636
Arkena <ads.neuroshok@gmail.com>
2023-04-06 00:59:05 +0200

    Revert "[update] after / before hooks don't require return type"
    This reverts commit 0a980992fda7eb6cd41751ea4eaa4b16b8d1d483.

--- include/ark/utility/function.hpp
+++ include/ark/utility/function.hpp
@@ -15,25 +15,8 @@ namespace ark
         using class_type = Class;
         using return_type = Return;
         using method_type = std::function<Return(Class*, Args...)>;
-        using noret_method_type = std::function<void(Class*, Args...)>;
         using flat_method_type = Return(*)(Class*, Args...);
         using functional_type = Return(Args...);
         using overwrite_method_type = std::function<Return(flat_method_type, Class*, Args...)>;
-    };
-
-    template<class T>
-    struct noret_function_trait : public function_trait<decltype(&std::decay_t<T>::operator())> {};
-
-    template<class Class, class Return, class... Args>
-    struct noret_function_trait<Return(Class::*)(Args...) const> : noret_function_trait<Return(Class::*)(Args...)>{};
-
-    template<class Class, class Return, class... Args>
-    struct noret_function_trait<Return(Class::*)(Args...)> {
-        using class_type = Class;
-        using return_type = void;
-        using method_type = std::function<void(Class*, Args...)>;
-        using flat_method_type = void(*)(Class*, Args...);
-        using functional_type = void(Args...);
-        using overwrite_method_type = std::function<void(flat_method_type, Class*, Args...)>;
     };
 } // ark
